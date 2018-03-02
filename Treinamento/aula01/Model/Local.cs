9b142e4a760d75083bafc108146155fbec4437c7
Elisângela Ferreira <elisange.f@hotmail.com>
2018-03-01 22:12:21 -0300

    First Commit

---
+++ Treinamento/aula01/Model/Local.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace aula01.Model
+{
+    class Local
+    {
+        public int Size { get; set; }
+
+        public string Code { get; set; }
+    }
+
+
+    class Sector: Local
+    {
+        public Farm[] Farms { get; set; }
+    }
+
+
+    class Farm : Local
+    {
+        public Block[] Blocks { get; set; }
+    }
+
+    class Block: Local
+    {
+        public Field[] Fields { get; set; }
+    }
+
+    class Field: Local
+    {
+        public string Culture { get; set; }
+
+        public void Plant(string culture)
+        {
+            Culture = culture;
+        }
+    }
+}
